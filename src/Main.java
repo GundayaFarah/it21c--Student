84cdeb9cabbf805a012c3764f314f753238f4a93
GundayaFarah <farahgundaya123@gmail.com>
2023-09-29 01:10:18 +0800

    added initial files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Student student = new Student("Farah I. Gundaya", "IT21C", "Be the change you wish to see in the world.");
+        
+        System.out.println("Name: " + student.getName());
+        System.out.println("Section: " + student.getSection());
+        System.out.println("Motto: " + student.getMotto());
+    }
+}
